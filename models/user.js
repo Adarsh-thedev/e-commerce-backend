92cf5a3fd48f52b9e14b453b707a0d7c6ac6dcf2
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-10 09:20:25 +0530

    user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuid = required(uuid/v1);
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -27,9 +29,9 @@ const userSchema = new Schema({
         trim :  true
     },
 
-    password : {
+    encry_password : {
         type : String,
-        trim :  true
+        required :  true
     },
 
     salt : String,
@@ -44,5 +46,35 @@ const userSchema = new Schema({
         default : []
     }
 });
+
+userSchema.virtual("password")
+    .set(function(password) {
+        this._password = password;
+        this.salt = uuidv1();
+        return rncry_password = this.securePassword(password);
+    })
+    .get(function() {
+        return this._password;
+    })
+
+userSchema.method = {
+    authenticate : function(plainpassword) {
+        return this,securePassword(plainpassword) === this.encry_password;
+    },
+
+    securePassword : function(plainpassword) {
+        if(!plainpassword) {
+            return '';
+        }
+
+        try {
+            return crypto.createHmac('sha256', this.salt)
+                .update(plainpassword)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema);
