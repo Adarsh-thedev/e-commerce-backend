7bc5e34c1c241960164e4fd4730ee5431e09d65f
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-11 18:25:20 +0530

    route

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-require('dotenv').config();
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+//DB Connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
@@ -14,6 +24,9 @@ mongoose.connect(process.env.DATABASE, {
     console.log('DB CONNECTED');
 })
 .catch(err=> console.log(err));
+
+//my routes
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=> {
