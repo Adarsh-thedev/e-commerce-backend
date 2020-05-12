a4a11e918bb5c9f4d9a65eff9262ed474058e411
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-12 10:20:06 +0530

    connected signup route with DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,18 @@
-exports.signOut = (req,res)=>{
+const User = require('../models/user');
+
+exports.signup = (req,res) => {
+    const user = new User(req.body);
+    user.save((err,user) => {
+        if(err) {
+            return res.status(400).json({
+                err : "Unable to save user"
+            })
+        }
+        res.json(user);
+    })
+}
+
+exports.signout = (req,res)=>{
     res.json({
         message : "User Signed Out",
         Time : Date.now()
