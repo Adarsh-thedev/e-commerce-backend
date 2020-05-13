267c15c464d0b3e7a013d532d44ae2a633e5da2c
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-13 20:48:29 +0530

    signup express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {signout, signup} = require('../controllers/auth');
+const { check } = require('express-validator');
 
-router.post('/signup', signup);
+router.post('/signup', [
+    check('name')
+    .isLength({ min: 3 }).withMessage('First name must be at least 3 chars long'),
+    check('email')
+    .isEmail().withMessage('Invalid email'),
+    check('password')
+    .isLength({ min: 8, max : 20 }).withMessage('Password must be at least 8-20 chars long')
+    .matches(/\d/).withMessage('Password must contain a number')
+] , signup);
 router.get('/signout', signout);
 
 module.exports = router;
