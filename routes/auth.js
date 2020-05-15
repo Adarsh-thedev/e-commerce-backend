0c3ef59dd47d53368822a6cd943380dd09f16bce
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-15 09:53:06 +0530

    signin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {signout, signup} = require('../controllers/auth');
+const {signout, signup, signin} = require('../controllers/auth');
 const { check } = require('express-validator');
 
 router.post('/signup', [
@@ -12,6 +12,14 @@ router.post('/signup', [
     .isLength({ min: 8, max : 20 }).withMessage('Password must be at least 8-20 chars long')
     .matches(/\d/).withMessage('Password must contain a number')
 ] , signup);
+
+router.post('/signin', [
+    check('email')
+    .isEmail().withMessage('Invalid email'),
+    check('password')
+    .isLength({ min: 1 }).withMessage('Password field is required')
+] , signin);
+
 router.get('/signout', signout);
 
 module.exports = router;
