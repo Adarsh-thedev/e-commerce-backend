3d8e9aaac8268d046e2999b904d7f981eb882690
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-16 22:05:11 +0530

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -19,3 +19,21 @@ exports.getUser = (req,res) => {
     req.profile.updatedAt = undefined;
     return res.json(req.profile);
 }
+
+exports.updateUser = (req,res) => {
+    User.findByIdAndUpdate(
+        {_id : req.profile._id},
+        {$set : req.body},
+        {new : true, useFindAndModify : false},
+        (err,user) => {
+            if(err) {
+                return res.status(400).json({
+                    error : "You are not authorized to perform this action"
+                })
+            }
+            user.salt = undefined;
+            ruser.encry_password = undefined;
+            res.json(user);
+        }
+    )
+}
