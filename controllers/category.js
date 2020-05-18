1d75474f4462487996f8bff203d73812f39bfd08
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-18 12:12:44 +0530

    create category

--- controllers/category.js
+++ controllers/category.js
@@ -11,3 +11,15 @@ exports.getCategoryById = (req,res,next,id) => {
         next();
     })
 }
+
+exports.createCatagory = (req,res) => {
+    const category = new Category(req.body);
+    category.save((err, category) => {
+        if(err || !category) {
+            return res.status(400).json({
+                error : "Unable to save category"
+            });
+        }
+        return res.json({category});
+    })
+}
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {getCategoryById} = require('../controllers/category');
+const {getCategoryById, createCatagory} = require('../controllers/category');
 const {getUserById} = require('../controllers/user');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 
+//params
 router.param('userId',getUserById);
 router.param('categroyId',getCategoryById);
+
+//actual routes
+router.post('/category/create/:userId', isSignedIn, isAuthenticated, isAdmin, createCatagory);
 
 module.exports = router;
