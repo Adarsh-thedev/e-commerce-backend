aebe2d103eed46ac81f479fd8d1536dc76d47b40
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-18 07:48:20 +0530

    user purchase list controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Order = require('../models/order');
 
 exports.getUserById = (req,res,next,id) => {
     User.findById(id).exec((err,user) => {
@@ -36,4 +37,17 @@ exports.updateUser = (req,res) => {
             res.json(user);
         }
     )
+}
+
+exports.userPurchaseList = (req,res) => {
+    Order.find({user : req.profile._id})
+    .populate('user', 'id name')
+    .exec((err,order) => {
+        if(err) {
+            return res.status(400).json({
+                error : "No order in this account"
+            });
+        }
+        return res.json(order);
+    });
 }
