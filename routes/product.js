8090588ef33c87dc72d91d968235b86e076b9fde
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-20 11:12:55 +0530

    update and delete routed

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo} = require('../controllers/product');
+const {getProductById, 
+        createProduct, 
+        getProduct, 
+        photo,
+        deleteProduct,
+        updateProduct
+    } = require('../controllers/product');
 const {getUserById} = require('../controllers/user');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 
@@ -10,8 +16,17 @@ router.param('productId',getProductById);
 router.param('userId',getUserById);
 
 //actual routes
+//create route
 router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+//read routes
 router.get('/product/:productId', getProduct);
 router.get('/product/photo/:productId', photo);
+
+//delete route
+router.delete('/product/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+
+//update
+router.put('/product/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, updateProduct);
 
 module.exports = router;
