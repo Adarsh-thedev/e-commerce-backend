2f61178e910fb21f42e3efdd836baeb4dafa58b8
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-20 11:12:23 +0530

    create and delete product

--- controllers/product.js
+++ controllers/product.js
@@ -17,6 +17,7 @@ exports.getProductById = (req,res,next,id) => {
     next();
 }
 
+//controllers
 exports.createProduct = (req,res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -72,6 +73,56 @@ exports.createProduct = (req,res) => {
 exports.getProduct = (req,res) => {
     req.product.photo = undefined;
     return res.json(req.product);
+}
+
+exports.deleteProduct = (req,res) => {
+    let product = req.product;
+    product.remove((err,deletedProduct) => {
+        if(err) {
+            return res.status(400).json({
+                error : "Unable to remove this product"
+            })
+        }
+        return res.json({
+            message : `${product.name} was deleted successfully!`
+        })
+    })
+}
+
+exports.updateProduct = (req,res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err,fields,file) => {
+        if(err) {
+            return res.status(400).json({
+                error : "An error occured"
+            })
+        }
+
+        //updation code
+        let product = req.product;
+        product = _.extend(product, fields);
+        if(file.photo) {
+            if(file.photo.size > (3 * 1024 * 1024)) {
+                return res.status(400).json({
+                    error : "Updation could not be performed"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+
+        //save to DB
+        product.save((err,product) => {
+            if(err) {
+                return res.status(400).json({
+                    error : "Failed to save product"
+                })
+            }
+            return res.json(`${product.name} was updated successfully!`);
+        });
+    })
 }
 
 //middleware
