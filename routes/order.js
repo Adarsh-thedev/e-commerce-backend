fff8b2f473a2ef2517b7811a9633dd1d12b9ef8f
Adarsh-thedev <adarshpittu@gmail.com>
2020-05-21 19:17:52 +0530

    create order and list all orders

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {getOrderById} = require('../controllers/order');
-const {getUserById} = require('../controllers/user');
+const {getOrderById, createOrder, getAllOrders} = require('../controllers/order');
+const {getUserById, pushOrderInPurchaseList} = require('../controllers/user');
 const {isSignedIn, isAuthenticated, isAdmin} = require('../controllers/auth');
 const {updateStock} = require('../controllers/product');
 
@@ -12,7 +12,18 @@ router.param('/:orderId', getOrderById);
 
 //actual routes
 //create
+router.post(
+    '/order/create/:userId', 
+    isSignedIn, 
+    isAuthenticated, 
+    pushOrderInPurchaseList, 
+    updateStock,
+    createOrder
+);
 
 //read
+router.get('/order/all/:userId', isSignedIn, isAuthenticated, isAdmin, getAllOrders);
+
+//status of order
 
 module.exports = router;
