0bcf5ba43a6da87523c83e6516fd9f22ac33dc3d
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-18 12:11:22 +0530

    stripe payment done

--- controllers/stripepayment.js
+++ controllers/stripepayment.js
@@ -1,9 +1,9 @@
-const stripe = require('stripe')('SECRET_KEY');
+const stripe = require('stripe')('sk_test_51GvGLoAjBsv7y60vtz9MXYJK8wuiIytdGBtOvO6YCcdmAQFK2qvB5CzJQq6zJN60T2ykxfX4ZNcuB4mkj77ej2mJ009C3kZPWm');
 const uuid = require('uuid/v4');
 
 exports.makePayment = (req,res) => {
 	const {products, token} = req.body;
-	console.log('PRODUCTS ', products);
+	console.log('TOKEN ', token);
 
 	let amount = 0;
 	products.map(p => {
@@ -18,11 +18,20 @@ exports.makePayment = (req,res) => {
 	})
 	.then(customer => {
 		stripe.charges.create({
-			amount : amount,
-			currency : 'INR',
+			amount : amount * 100,
+			currency : 'inr',
+			customer : customer.id,
 			receipt_email : token.email,
+			description : 'Payment for t-shirt',
 			shipping : {
-				name : token.card.name
+				name : token.card.name,
+				address : {
+					line1 : token.card.address_line1,
+					line2 : token.card.address_line2,
+					city : token.card.address_city,
+					country : token.card.address_country,
+					postal_code : token.card.address_zip
+				}
 			}
 		}, {idempotencyKey})
 		.then(result => res.status(200).json(result))
