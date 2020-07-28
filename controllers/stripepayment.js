340962acd258c9bc7a2a7a0dfca9d60fe400a309
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-28 09:37:06 +0530

    key in .env

--- controllers/stripepayment.js
+++ controllers/stripepayment.js
@@ -1,40 +1,45 @@
-const stripe = require('stripe')('sk_test_51GvGLoAjBsv7y60v365eihnn1ZmpFTp98zdJ1Uoqvt13hQnweNriFoAWn46DPFyo93qnbPtAoU5Qr2IG0MEzBHER00ZFV035Zv');
-const uuid = require('uuid/v4');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const uuid = require("uuid/v4");
 
-exports.makePayment = (req,res) => {
-	const {products, token} = req.body;
-	console.log('TOKEN ', token);
+exports.makePayment = (req, res) => {
+  const { products, token } = req.body;
+  console.log("TOKEN ", token);
 
-	let amount = 0;
-	products.map(p => {
-		amount = amount + p.price;
-	})
+  let amount = 0;
+  products.map((p) => {
+    amount = amount + p.price;
+  });
 
-	const idempotencyKey = uuid();
+  const idempotencyKey = uuid();
 
-	return stripe.customers.create({
-		email : token.email,
-		source : token.id
-	})
-	.then(customer => {
-		stripe.charges.create({
-			amount : amount * 100,
-			currency : 'inr',
-			customer : customer.id,
-			receipt_email : token.email,
-			description : 'Payment for t-shirt',
-			shipping : {
-				name : token.card.name,
-				address : {
-					line1 : token.card.address_line1,
-					line2 : token.card.address_line2,
-					city : token.card.address_city,
-					country : token.card.address_country,
-					postal_code : token.card.address_zip
-				}
-			}
-		}, {idempotencyKey})
-		.then(result => res.status(200).json(result))
-		.catch(err => console.log(err))
-	})
-}
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges
+        .create(
+          {
+            amount: amount * 100,
+            currency: "inr",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: "Payment for t-shirt",
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip,
+              },
+            },
+          },
+          { idempotencyKey }
+        )
+        .then((result) => res.status(200).json(result))
+        .catch((err) => console.log(err));
+    });
+};
