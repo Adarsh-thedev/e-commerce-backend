b860308792a512ab486bdf7a97cacd79950481a9
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-28 09:43:15 +0530

    deploying

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 //my routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const orderRoutes = require('./routes/order');
-const stripeRoutes = require('./routes/stripepayment');
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment");
 
 const app = express();
 
@@ -21,25 +21,25 @@ app.use(cookieParser());
 app.use(cors());
 
 //DB Connection
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true, 
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex : true
-})
-.then(()=>{
-    console.log('DB CONNECTED');
-})
-.catch(err=> console.log(err));
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => console.log(err));
 
 //my routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', orderRoutes);
-app.use('/api', stripeRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, ()=> {
-    console.log(`App is running on PORT ${PORT}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`App is running on PORT ${process.env.PORT}`);
 });
